81b46da9d4178faced0e992283b6fb6c76fd281f
warrantyvoidifremoved <rahulssrinath@gmail.com>
2026-04-08 07:23:21 -0700

    Fixed path for background.png

--- application/frontend/src/auth/Signup.jsx
+++ application/frontend/src/auth/Signup.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import signupImage from "./assets/signup/background.png";
+import signupImage from "../assets/signup/background.png";
 
 export default function Signup() {
     const navigate = useNavigate();
