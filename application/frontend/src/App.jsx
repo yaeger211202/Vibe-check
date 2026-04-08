8ccf2da1cf778d5b36e1de9ca2a480f0fa24fb2f
warrantyvoidifremoved <rahulssrinath@gmail.com>
2026-04-08 02:19:49 -0700

    Added route for sign up page

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import OurTeam from "./OurTeam.jsx";
 import VibeCheckLandingPage from "./LandingPage.jsx";
+import Signup from "./signup.jsx";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<VibeCheckLandingPage />} />
             <Route path="/team" element={<OurTeam />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     );
 }
