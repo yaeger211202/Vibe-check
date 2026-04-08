3a4ffb860f356f662fac0bdce40ef2c8622cac66
warrantyvoidifremoved <rahulssrinath@gmail.com>
2026-04-08 01:20:17 -0700

    Add routing for pages

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import About from "./about/About.jsx";
+import VibeCheckLandingPage from "./landing_page/LandingPage.jsx";
 
 export default function App() {
     return (
-        <div>
-            <About />
-        </div>
+        <Routes>
+            <Route path="/" element={<VibeCheckLandingPage />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
     );
 }
