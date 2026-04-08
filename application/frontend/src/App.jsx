8679ab47b8d76e4669eb0ef391b49c6f3e874937
warrantyvoidifremoved <rahulssrinath@gmail.com>
2026-04-08 07:21:33 -0700

    Added routes for signout and profile pages

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./Landing.jsx";
 import Map from "./Map.jsx";
-import Signin from "./Signin.jsx";
-import Signup from "./Signup.jsx";
+import Signin from "./auth/Signin.jsx";
+import Signup from "./auth/Signup.jsx";
+import Signout from "./auth/Signout.jsx";
 import OurTeam from "./OurTeam.jsx";
+import Profile from "./Profile.jsx";
+
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/map" element={<Map />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/team" element={<OurTeam />} />
             <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signout" element={<Signout />} />
+            <Route path="/team" element={<OurTeam />} />
+            <Route path="/profile" element={<Profile />} />
         </Routes>
     );
 }
