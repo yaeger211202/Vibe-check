0e63ecf6bb84fe2158095e0537bc7dcb90c9af23
warrantyvoidifremoved <rahulssrinath@gmail.com>
2026-05-12 23:22:19 -0700

    Fix import for userRoutes

--- application/backend/server.js
+++ application/backend/server.js
@@ -12,7 +12,7 @@ import { createNotesRoutes } from './routes/notesRoutes.js';
 import { createReactionsRoutes } from './routes/reactionsRoutes.js';
 import { createRepliesRoutes } from './routes/repliesRoutes.js';
 import { createLocationsRoutes } from './routes/locationsRoutes.js';
-import { createUsersRoutes } from './routes/usersRoutes.js';
+import { createUsersRoutes } from './routes/userRoutes.js';
 import swaggerSpec from "./docs/openapi.js";
 
 
